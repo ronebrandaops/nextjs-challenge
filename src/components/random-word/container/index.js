724bdbe6b002076c3be5a23fccfb66e2785ccf30
Rone Brandao <rcbrandao@pagseguro.com>
2023-08-01 19:08:31 -0300

    refactor: remove alias

--- src/components/random-word/container/index.js
+++ src/components/random-word/container/index.js
@@ -7,20 +7,16 @@ import { selectRandomWord } from "../selectors";
 import RandomWord from "../presentation";
 
 const RandomWordContainer = () => {
-  // Pega referência do dispatch da store
   const dispatch = useDispatch();
 
-  // Cria o handler de ação
   const handler = useCallback(() => {
     dispatch(fetchRandomWord());
   }, [dispatch]);
 
-  // Chama a ação assim que o componente é montado
   useEffect(handler, []);
 
-  // Seleciona os dados da store
   const data = useSelector(selectRandomWord);
-  console.log("data", data);
+
   return <RandomWord {...data} onClick={handler} />;
 };
 
